44e5e5164a7daf8f6aefe4d2ed2ccf70ee62c809
huangyong <369107259@qq.com>
2018-08-06 18:34:19 +0800

    commit

---
+++ src/main/java/com/spring/boot/config/RedisComponent.java
@@ -0,0 +1,121 @@
+package com.spring.boot.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.SetOperations;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+
+@Component("redisComponent")
+public class RedisComponent {
+    private static final Logger logger = LoggerFactory.getLogger(RedisComponent.class);
+
+//    @Autowired
+//    private RedisTemplate<String, Object> redisTemplate;
+    @Autowired
+    private HashOperations<String, String, String> hashOperation;
+    @Autowired
+    private ListOperations<String, String> listOperation;
+    @Autowired
+    private SetOperations<String, String> setOperation;
+
+
+    public Object hGet(String key, String hKey){
+        return hashOperation.get(key, hKey);
+    }
+
+    public List<String> hGet(String key, Collection<String> hKeys){
+        return hashOperation.multiGet(key, hKeys);
+    }
+
+    public Map<String, String> hGetAll(String key){
+        return hashOperation.entries(key);
+    }
+
+    public void hPut(String key, String hKey, String value){
+        logger.debug("hPut key:{}, hKey:{}, value:{}", key, hKey, value);
+        hashOperation.put(key, hKey, value);
+    }
+
+    public void hRemove(String key, String... hKeys){
+        logger.debug("hRemove key:{}, hKeys:{}", key, hKeys);
+        if(hKeys != null && hKeys.length > 0){
+            Object[] objects = new Object[hKeys.length];
+            for(int i = 0; i < hKeys.length; i++){
+                objects[i] = hKeys[i];
+            }
+            hashOperation.delete(key, objects);
+        }
+    }
+
+    public void leftPush(String key, String value){
+        logger.debug("leftPush key:{}, value:{}", key, value);
+        listOperation.leftPush(key, value);
+    }
+
+    public void rightPush(String key, String value){
+        logger.debug("rightPush key:{}, value:{}", key, value);
+        listOperation.rightPush(key, value);
+    }
+
+    public Object leftPop(String key){
+        Object value = listOperation.leftPop(key);
+        logger.debug("leftPop key:{}, value:{}", key, value);
+        return value;
+    }
+
+    public Object rightPop(String key){
+        Object value = listOperation.rightPop(key);
+        logger.debug("rightPop key:{}, value:{}", key, value);
+        return value;
+    }
+
+    public List<String> lGetAll(String key){
+        Long size = lSize(key);
+        if(size == null || size == 0) return new ArrayList<>();
+        return lGetRange(key, 0, size - 1);
+    }
+
+    public List<String> lGetRange(String key, long start, long end){
+        return listOperation.range(key, start, end);
+    }
+
+    public Long lSize(String key){
+        return listOperation.size(key);
+    }
+
+    public void sAdd(String key, String... values){
+        logger.debug("sAdd key:{}, values:{}", key, values);
+        setOperation.add(key, values);
+    }
+
+    public void sRemove(String key, String... values){
+        logger.debug("sRemove key:{}, values:{}", key, values);
+        if(values != null && values.length > 0){
+            Object[] objects = new Object[values.length];
+            for(int i = 0; i < values.length; i++){
+                objects[i] = values[i];
+            }
+            setOperation.remove(key, objects);
+        }
+    }
+
+    public void lRemove(String key, int count, Object value){
+        logger.debug("lRemove key:{}, count:{}, value:{}", key, count, value);
+        listOperation.remove(key, count, value);
+    }
+
+    public Object sPop(String key){
+        Object value = setOperation.pop(key);
+        logger.debug("sPop key:{}, value:{}", key, value);
+        return value;
+    }
+
+    public Set<String> sGetAll(String key){
+        return setOperation.members(key);
+    }
+}
---
+++ src/main/java/com/spring/boot/config/RedisConfig.java
@@ -0,0 +1,112 @@
+package com.spring.boot.config;
+
+import com.yx.orderservice.task.CacheRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.*;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * Created by liulinbao on 2017/7/20.
+ */
+
+@Configuration
+public class RedisConfig {
+    @Autowired
+    private RedisConnectionFactory redisConnectionFactory;
+
+    /**
+     * 实例化 RedisTemplate 对象
+     *
+     * @return
+     */
+    @Bean
+    public RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        initDomainRedisTemplate(redisTemplate, redisConnectionFactory);
+        return redisTemplate;
+    }
+
+    /**
+     * 设置数据存入 redis 的序列化方式
+     *
+     * @param redisTemplate
+     * @param factory
+     */
+    private void initDomainRedisTemplate(RedisTemplate<String, String> redisTemplate, RedisConnectionFactory factory) {
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        //redisTemplate.setHashValueSerializer(new JdkSerializationRedisSerializer());
+       // redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
+        redisTemplate.setConnectionFactory(factory);
+    }
+
+    /**
+     * 实例化 HashOperations 对象,可以使用 Hash 类型操作
+     *
+     * @param redisTemplate
+     * @return
+     */
+    @Bean
+    public HashOperations<String, String, String> hashOperation(RedisTemplate<String, String> redisTemplate) {
+        return redisTemplate.opsForHash();
+    }
+
+    /**
+     * 实例化 ValueOperations 对象,可以使用 String 操作
+     *
+     * @param redisTemplate
+     * @return
+     */
+    @Bean
+    public ValueOperations<String, String> valueOperation(RedisTemplate<String, String> redisTemplate) {
+        return redisTemplate.opsForValue();
+    }
+
+    /**
+     * 实例化 ListOperations 对象,可以使用 List 操作
+     *
+     * @param redisTemplate
+     * @return
+     */
+    @Bean
+    public ListOperations<String, String> listOperation(RedisTemplate<String, String> redisTemplate) {
+        return redisTemplate.opsForList();
+    }
+
+    /**
+     * 实例化 SetOperations 对象,可以使用 Set 操作
+     *
+     * @param redisTemplate
+     * @return
+     */
+    @Bean
+    public SetOperations<String, String> setOperation(RedisTemplate<String, String> redisTemplate) {
+        return redisTemplate.opsForSet();
+    }
+
+    /**
+     * 实例化 ZSetOperations 对象,可以使用 ZSet 操作
+     *
+     * @param redisTemplate
+     * @return
+     */
+    @Bean
+    public ZSetOperations<String, String> zSetOperation(RedisTemplate<String, String> redisTemplate) {
+        return redisTemplate.opsForZSet();
+    }
+
+    /**
+     * 初始化缓存cache
+     * @return
+     */
+    @Bean
+    public CacheRunner cacheRunner(){
+        return new CacheRunner();
+    }
+
+}

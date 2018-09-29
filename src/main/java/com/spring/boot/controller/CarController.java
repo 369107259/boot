195743aaedc97738c96037398158ef52618502bd
huangyong <369107259@qq.com>
2018-09-29 17:33:17 +0800

    commit

--- src/main/java/com/spring/boot/controller/CarController.java
+++ src/main/java/com/spring/boot/controller/CarController.java
@@ -1,6 +1,8 @@
 package com.spring.boot.controller;
 
 import com.spring.boot.service.CarService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  **/
 @Controller
 public class CarController {
+    private static final Logger logger = LoggerFactory.getLogger(CarController.class);
     @Autowired
     private CarService carService;
 
